e4901ab0a1e11e567e37423efda1ea192e2cd94e
Anton Serdyuchenko <anton415@gmail.com>
2024-12-08 14:13:38 +0300

    Initial Commit

---
+++ Conversions/ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionsApp.swift
+//  Conversions
+//
+//  Created by Anton Serdyuchenko on 08.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
